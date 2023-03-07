7945da0df0f216049235782fc67e8c9aba258176
Patrick Gratias <johnpatrickpreston84@gmail.com>
2023-03-07 18:13:17 +0100

    clipboard is working, just some minor changes in design necessary

--- src/components/bitly.jsx
+++ src/components/bitly.jsx
@@ -1,26 +1,25 @@
 import React, { useState } from "react";
-import axios from "axios";
+import "./bitly.css";
 
 function Bitly() {
   const [longUrl, setLongUrl] = useState("");
   const [shortUrl, setShortUrl] = useState("");
 
-  const handleClick = () => {
-    axios
-      .post(
-        "https://api-ssl.bitly.com/v4/shorten",
-        {
-          long_url: longUrl,
-        },
-        {
-          headers: {
-            Authorization: `Bearer 73e7701cb6e9bbc4ad232de321dd59a730152b34`,
-            "Content-Type": "application/json",
-          },
-        }
-      )
-      .then((response) => {
-        setShortUrl(response.data.link);
+  const handleClickToShort = () => {
+    console.log(longUrl);
+    fetch("https://api-ssl.bitly.com/v4/shorten", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer 73e7701cb6e9bbc4ad232de321dd59a730152b34`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        long_url: longUrl,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setShortUrl(data.link);
       })
       .catch((error) => {
         console.log(error);
@@ -28,19 +27,40 @@ function Bitly() {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={longUrl}
-        onChange={(event) => setLongUrl(event.target.value)}
-      />
-      <button onClick={handleClick}>Shorten URL</button>
-      {shortUrl && (
-        <a href={shortUrl} target="_blank" rel="noopener noreferrer">
-          {shortUrl}
-        </a>
-      )}
-    </div>
+    <section>
+      <div className="form-box">
+        <div className="form-value">
+          <form action="">
+            <h2>Short URL</h2>
+            <div className="inputbox">
+            <input
+              type="text"
+              value={longUrl}
+              onChange={(event) => setLongUrl(event.target.value)}
+            />
+            <label htmlFor="">Long URL</label>
+            </div>
+            <button type="button" onClick={handleClickToShort}>Shorten URL</button>
+            {shortUrl && (
+              <div className="inputbox">
+                <input 
+                  className="shorturl" 
+                  type="text"
+                  value={shortUrl}
+                />
+                <div className="formbutton">
+                  <input 
+                    type="button"
+                    value="Copy URL"
+                    onclick={navigator.clipboard.writeText(shortUrl)}
+                    />
+                </div>
+              </div>
+            )}
+          </form>
+        </div>
+      </div>
+    </section>
   );
 }
 
